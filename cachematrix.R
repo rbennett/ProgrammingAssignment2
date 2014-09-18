06e67238ee03b4e9f4cd09ff1aa96a63e9ad0ea2
rich <njengineer@ml1.net>
2014-09-18 03:42:45 -0400

    Assignment 2 matrix cache functions coded and tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to manage a cache of inverted matrices.   
+## These can be be used to eliminated duplicate computation and
+## hide the logic for first time calculation and cache access
+## from the calling program
 
-## Write a short comment describing this function
+## Create a list of methods to cache a matrix and inverted solution
 
 makeCacheMatrix <- function(x = matrix()) {
+        inverted_x  <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                inverted_x <<- NULL
+        }
+        
+        get <- function() x
+        
+        setim <- function(im = matrix) inverted_x <<- im
+        
+        getim <- function() inverted_x
+        
+        
+        list(set = set, 
+             get = get,
+             setim = setim,
+             getim = getim     )
+        }
 
-}
 
 
-## Write a short comment describing this function
+## Function used to access cache.
+## On first access solve and cache the inverted matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        im <- x$getim()
+        if(is.matrix(im)) {
+                message("matrix cache hit")
+                return(im)
+        }
+        message("first time matrix inversion")
+        mx <- x$get()
+        im <- solve(mx, ...)
+        x$setim(im)
+        im
+        }
